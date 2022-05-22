e2cfab3d45f12f84cda5f74f187c8de6b9c04742
Kamil Kozak <130604@student.uni.opole.pl>
2022-05-22 21:39:56 +0200

    Poprawiony panel logowania

--- HotelDlaPsow/MainWindow.xaml.cs
+++ HotelDlaPsow/MainWindow.xaml.cs
@@ -20,16 +20,53 @@ namespace HotelDlaPsow
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int loginFailed = 0;
+        private int loginLeft = 3;
+        private MessageBoxResult result;
+
         public MainWindow()
         {
             InitializeComponent();
             
         }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private void ButtonLogin_Click(object sender, RoutedEventArgs e)
         {
-            WindowLogin log = new WindowLogin();
-            log.Show();
+            WindowMenu menu = new WindowMenu();
+                if (TextBoxLogin.Text == "a")
+                {
+                    if (PasswordBoxPass.Password == "a")
+                    {
+                        menu.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        loginLeft--;
+                        MessageBox.Show("Niepoprawne hasło! Pozostało " + loginLeft + " prób logowania.", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        loginFailed++;
+                    }
+                }
+                else
+                {
+                    loginLeft--;
+                    MessageBox.Show("Niepoprawny login! Pozostało " + loginLeft + " prób logowania.", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    loginFailed++;
+                }
+            if (loginFailed >= 3)
+            {
+               result = MessageBox.Show("Konto zostało zablokowane!","Error",MessageBoxButton.OK,MessageBoxImage.Error);
+                switch (result) {
+                    case MessageBoxResult.OK:
+                        Environment.Exit(loginFailed);
+                    break;
+                }
+            }
+        }
+
+        private void HandleHandleInput (object sender, TextCompositionEventArgs e) {
+           
+                e.Handled = true;
+            
         }
     }
 }
